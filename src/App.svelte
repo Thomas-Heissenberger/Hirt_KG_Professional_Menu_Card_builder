9d1e0317369eb1b7772102aef4a51a870099e863
Thomas Heißenberger <64338399+Thomas-Heissenberger@users.noreply.github.com>
2022-01-28 22:03:42 +0100

    cool 😋

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,33 @@
 <script lang="ts">
-	import "../node_modules/svelte-material-ui/bare.css";
-	export let name: string;
-	import Textfield from '@smui/textfield';
-  import Icon from '@smui/textfield/icon';
-  import HelperText from '@smui/textfield/helper-text';
- 
+  import "../node_modules/svelte-material-ui/bare.css";
+  export let name: string;
+  import Textfield from "@smui/textfield";
+  import Icon from "@smui/textfield/icon";
+  import HelperText from "@smui/textfield/helper-text";
+  import SelectMenu from "./SelectMenu.svelte";
+  import Button, {Label} from '@smui/button';
+
   let focused = false;
   let value: string | null = null;
   let dirty = false;
   let invalid = false;
   $: disabled = focused || !value || !dirty || invalid;
- 
+  let t=123;
   function clickHandler() {
     alert(`Sending to ${value}!`);
     value = null;
     dirty = false;
   }
+
 </script>
-<div class="margins">
-	<!--
+
+<Button on:click={()=>t++}>
+	<Label>Click Me!</Label>
+</Button>	
+<h1>{t}</h1>
+  <!--
 	  Note: when you bind to `invalid`, but you only want to
 	  monitor it instead of updating it yourself, you also
 	  should include `updateInvalid`.
 	-->
-	<Textfield
-	  type="email"
-	  bind:dirty
-	  bind:invalid
-	  updateInvalid
-	  bind:value
-	  label="To"
-	  style="min-width: 250px;"
-	  input$autocomplete="email"
-	  on:focus={() => (focused = true)}
-	  on:blur={() => (focused = false)}
-	  withTrailingIcon={!disabled}
-	>
-	  <!--
-		Since this icon is conditional, it needs to be wrapped
-		in a fragment, and we need to provide withTrailingIcon.
-	  -->
-	  <svelte:fragment slot="trailingIcon">
-		{#if !disabled}
-		  <Icon class="material-icons" role="button" on:click={clickHandler}
-			>send</Icon
-		  >
-		{/if}
-	  </svelte:fragment>
-	  <HelperText validationMsg slot="helper">
-		That's not a valid email address.
-	  </HelperText>
-	</Textfield>
-  </div>
-   
-  <pre
-	class="status">Focused: {focused}, Dirty: {dirty}, Invalid: {invalid}, Value: {value}</pre>
+  <SelectMenu/>
