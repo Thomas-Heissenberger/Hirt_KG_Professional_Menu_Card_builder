1e3e1cf74c5f1a7316680c48d5b1ccac1a2c2d08
Thomas Heißenberger <64338399+Thomas-Heissenberger@users.noreply.github.com>
2022-01-28 21:48:33 +0100

    leiwand oida 😱

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,57 @@
 <script lang="ts">
-
+	import "../node_modules/svelte-material-ui/bare.css";
 	export let name: string;
-
+	import Textfield from '@smui/textfield';
+  import Icon from '@smui/textfield/icon';
+  import HelperText from '@smui/textfield/helper-text';
+ 
+  let focused = false;
+  let value: string | null = null;
+  let dirty = false;
+  let invalid = false;
+  $: disabled = focused || !value || !dirty || invalid;
+ 
+  function clickHandler() {
+    alert(`Sending to ${value}!`);
+    value = null;
+    dirty = false;
+  }
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
-
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="margins">
+	<!--
+	  Note: when you bind to `invalid`, but you only want to
+	  monitor it instead of updating it yourself, you also
+	  should include `updateInvalid`.
+	-->
+	<Textfield
+	  type="email"
+	  bind:dirty
+	  bind:invalid
+	  updateInvalid
+	  bind:value
+	  label="To"
+	  style="min-width: 250px;"
+	  input$autocomplete="email"
+	  on:focus={() => (focused = true)}
+	  on:blur={() => (focused = false)}
+	  withTrailingIcon={!disabled}
+	>
+	  <!--
+		Since this icon is conditional, it needs to be wrapped
+		in a fragment, and we need to provide withTrailingIcon.
+	  -->
+	  <svelte:fragment slot="trailingIcon">
+		{#if !disabled}
+		  <Icon class="material-icons" role="button" on:click={clickHandler}
+			>send</Icon
+		  >
+		{/if}
+	  </svelte:fragment>
+	  <HelperText validationMsg slot="helper">
+		That's not a valid email address.
+	  </HelperText>
+	</Textfield>
+  </div>
+   
+  <pre
+	class="status">Focused: {focused}, Dirty: {dirty}, Invalid: {invalid}, Value: {value}</pre>
